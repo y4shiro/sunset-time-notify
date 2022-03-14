b333bb90eaeea2e3724b2e2ff20e24c4ef8a1477
y4shiro <y4shironao@gmail.com>
2022-03-14 19:56:46 +0900

    main コンテンツ仮置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1 className='text-3xl font-bold underline text-red-400'>Next.js!</h1>;
+  return (
+    <div className='h-96 flex items-center justify-center'>
+      <h1 className='text-4xl font-bold text-gray-600'>Next.js!</h1>
+    </div>
+  );
 };
 
 export default Home;
