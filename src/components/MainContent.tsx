6063e21dd755c71492836d668c6e3f355e5de739
y4shiro <y4shironao@gmail.com>
2022-03-19 21:06:10 +0900

    SunsetInfo コンポーネント作成

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,13 @@
 import React, { VFC } from 'react';
+
 import Map from './Map';
+import SunsetInfo from './SunsetInfo';
 
 const MainContent: VFC = () => {
   return (
     <div className='flex flex-wrap'>
       <Map />
-      {/* <div>日没情報コンポーネント</div> */}
+      <SunsetInfo />
     </div>
   );
 };
---
+++ src/components/SunsetInfo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SunsetInfo = () => {
+  return <div>日没情報コンポーネント</div>;
+};
+
+export default SunsetInfo;
