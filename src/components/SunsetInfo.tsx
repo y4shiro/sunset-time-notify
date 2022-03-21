ecb2f7e357c41ce9e1a59f0b203bdbf42b813e6b
y4shiro <y4shironao@gmail.com>
2022-03-21 22:06:43 +0900

    SunsetInfo にカードスタイルを追加

--- src/components/SunsetInfo.tsx
+++ src/components/SunsetInfo.tsx
@@ -1,7 +1,29 @@
 import { VFC } from 'react';
 
 const SunsetInfo: VFC = () => {
-  return <div className='h-96 w-full bg-red-100'>日没情報コンポーネント</div>;
+  return (
+    <div className='w-full bg-red-100'>
+      <div className='m-6 p-6 max-h-full bg-white rounded-lg border border-gray-200 shadow-md'>
+        <h5>SunsetInfo カード</h5>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+        <p>時刻を入力</p>
+      </div>
+    </div>
+  );
 };
 
 export default SunsetInfo;
