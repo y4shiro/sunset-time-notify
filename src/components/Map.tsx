e50481889506f07934bd4bc36f7465a580f314a5
y4shiro <y4shironao@gmail.com>
2022-03-24 22:48:32 +0900

    Map コンポーネントに仮の地図画像追加

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,21 @@
 import { VFC } from 'react';
+import Image from 'next/image';
+
+import MapImg from '../../public/Map.png';
 
 const Map: VFC = () => {
-  return <div className='w-full bg-green-100'>Mapコンポーネント</div>;
+  return (
+    <div className='w-full bg-green-100'>
+      <Image
+        src='/Map.png'
+        width='100'
+        height='100%'
+        layout='responsive'
+        objectFit='contain'
+        alt='Map'
+      />
+    </div>
+  );
 };
 
 export default Map;
