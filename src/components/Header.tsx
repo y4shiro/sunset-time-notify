d2f4dd0a7d82de4761f90f56e025289247c24ba1
y4shiro <y4shironao@gmail.com>
2022-03-27 19:22:52 +0900

    Header のスタイリングを Chakra UI に差し替え

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,30 @@
 import React, { VFC } from 'react';
+import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react';
+
+import NextLink from 'next/link';
 
 const Header: VFC = () => {
   return (
-    <nav className='bg-sky-700 border-gray-200 px-2 py-2 rounded'>
-      <div className='container flex flex-wrap items-center justify-between mx-auto'>
-        <a href='#'>
-          <h1 className='py-4 px-4 text-2xl text-gray-100'>Sunset Time Notify</h1>
-        </a>
-        <div className='flex'>
-          <a href='#'>
-            <span className='py-4 px-4 text-gray-100 hover:underline'>about</span>
-          </a>
-          <a href='#'>
-            <span className='py-4 px-4 text-gray-100 hover:underline'>menu</span>
-          </a>
-        </div>
-      </div>
-    </nav>
+    <Box as='nav' p={2} bgColor={'blue.600'} rounded={'md'}>
+      <Flex px={4} alignItems={'center'} justify={'space-between'}>
+        <NextLink href='#' passHref>
+          <Link textColor={'gray.100'} _hover={{ textDecorationLine: 'none' }}>
+            <Heading as={'h1'} py={4} size={'lg'}>
+              Sunset Time Notify
+            </Heading>
+          </Link>
+        </NextLink>
+
+        <Flex gap={8}>
+          <Link href='#' textColor={'gray.100'}>
+            <Text>about</Text>
+          </Link>
+          <Link href='#' textColor={'gray.100'}>
+            <Text>menu</Text>
+          </Link>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
 
