ab8d4da27b1c7b45039a64c541cc341f97142bfc
y4shiro <y4shironao@gmail.com>
2022-03-28 17:44:38 +0900

    MainContent のスタイリングを Chakra UI に差し替え

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,14 +1,15 @@
 import { VFC } from 'react';
+import { Flex } from '@chakra-ui/react';
 
 import Map from './Map';
 import SunsetInfo from './SunsetInfo';
 
 const MainContent: VFC = () => {
   return (
-    <main className='flex'>
+    <Flex>
       <Map />
       <SunsetInfo />
-    </main>
+    </Flex>
   );
 };
 
