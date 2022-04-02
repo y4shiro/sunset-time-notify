2b22bde6e39877d842d369c29ecd73ec9dd14f9d
y4shiro <y4shironao@gmail.com>
2022-04-02 22:37:38 +0900

    インプットフォーム部分をコンポーネント分割

--- src/components/SunsetInfo/index.tsx
+++ src/components/SunsetInfo/index.tsx
@@ -12,6 +12,7 @@ import {
 } from '@chakra-ui/react';
 
 import { CurrentTime } from './CurrentTime';
+import { InputForm } from './InputForm';
 
 const SunsetInfo: VFC = () => {
   return (
@@ -39,34 +40,7 @@ const SunsetInfo: VFC = () => {
           </Box>
         </HStack>
 
-        <FormControl>
-          <Box my={4}>
-            <FormLabel htmlFor='countries'>国名を選んでください</FormLabel>
-            <Select id='countries'>
-              <option value='United States'>United States</option>
-              <option value='Canada'>Canada</option>
-              <option value='France'>France</option>
-              <option value='Germany'>Germany</option>
-            </Select>
-          </Box>
-
-          <Box my={4}>
-            <FormLabel htmlFor='text1'>フォーム1</FormLabel>
-            <Input id='text1' type='text' placeholder='フォーム1' />
-          </Box>
-          <Box my={4}>
-            <FormLabel htmlFor='text2'>フォーム2</FormLabel>
-            <Input id='text2' type='text' placeholder='フォーム2' />
-          </Box>
-          <Box my={4}>
-            <FormLabel htmlFor='text3'>フォーム3</FormLabel>
-            <Input id='text3' type='text' placeholder='フォーム3' />
-          </Box>
-        </FormControl>
-
-        <Button colorScheme='blue' type='submit'>
-          計算開始
-        </Button>
+        <InputForm />
       </Box>
     </Box>
   );
