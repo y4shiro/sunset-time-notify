43b49373f9f10a93579eb3f08d99756652cd87cb
y4shiro <y4shironao@gmail.com>
2022-04-04 18:28:07 +0900

    現在時刻の hours のフォーマット指定を 24 時間制に変更

--- src/hooks/useCurrentTime.ts
+++ src/hooks/useCurrentTime.ts
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import format from 'date-fns/format';
 
 export const useCurrentTime = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const [startTime, setStartTime] = useState(Date.now());
+  const [currentTime, setCurrentTime] = useState(Date.now());
   const [currentTimeString, setCurrentTimeString] = useState('');
 
-  const updateTime = () => {
-    const currentDate = new Date();
+  const updateTime = (currentDate = Date.now()) => {
     setCurrentTime(currentDate);
-    setCurrentTimeString(format(currentDate, 'yyyy年MM月dd日 hh:mm:ss'));
+    setCurrentTimeString(format(currentDate, 'yyyy年MM月dd日 HH:mm:ss'));
   };
 
   useEffect(() => {
@@ -16,14 +16,15 @@ export const useCurrentTime = () => {
   }, []);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
+    const intervalId = setInterval(() => {
+      const diff = Date.now() - startTime;
       updateTime();
     }, 1000);
 
     return () => {
-      clearTimeout(timeoutId);
+      clearInterval(intervalId);
     };
-  }, [currentTime]);
+  }, []);
 
   return { currentTime, currentTimeString };
 };
