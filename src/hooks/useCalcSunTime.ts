12a45c0cdd169a5c13eddf03a00e6e08b0d2bcd8
y4shiro <y4shironao@gmail.com>
2022-04-06 18:44:42 +0900

    日の出日の入時刻のフォーマットを修正

--- src/hooks/useCalcSunTime.ts
+++ src/hooks/useCalcSunTime.ts
@@ -1,10 +1,12 @@
 import SunCalc from 'suncalc';
+import format from 'date-fns/format';
 
 export const useCalcSunTime = (lat: number, lon: number, alt: number) => {
   const correctAlt = alt < 0 ? 0 : alt; // suncalc は alt(高度) 0m 未満に対応していないので、その場合は 0m として扱う
   const times = SunCalc.getTimes(new Date(), lat, lon, correctAlt); // @Types の型定義と SunCalc の実装が異なるので、src/types.d.ts で型定義拡張を行った
-  const sunriseTime = times.sunrise.toLocaleTimeString();
-  const sunsetTime = times.sunset.toLocaleTimeString();
+
+  const sunriseTime = format(times.sunrise, 'HH:mm:ss');
+  const sunsetTime = format(times.sunset, 'HH:mm:ss');
 
   return { sunriseTime, sunsetTime };
 };
