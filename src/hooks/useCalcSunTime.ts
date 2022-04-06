1fc627709ce49c3958f903bb22e6c7d54e08d5e7
y4shiro <y4shironao@gmail.com>
2022-04-06 16:33:39 +0900

    現在利用している日の出日の入時刻計算ライブラリは高度は対象外なので、高度にも対応している SunCalc に差し替えた

--- src/hooks/useCalcSunTime.ts
+++ src/hooks/useCalcSunTime.ts
@@ -1,8 +1,11 @@
-import { getSunrise, getSunset } from 'sunrise-sunset-js';
+import SunCalc from 'suncalc';
 
 export const useCalcSunTime = (lat: number, lon: number, alt: number) => {
-  const sunriseTime = getSunrise(lat, lon, new Date()).toLocaleTimeString();
-  const sunsetTime = getSunset(lat, lon, new Date()).toLocaleTimeString();
+  const correctAlt = alt < 0 ? 0 : alt; // suncalc は alt(高度) 0m 未満に対応していないので、その場合は 0m として扱う
+  // @ts-ignore
+  const times = SunCalc.getTimes(new Date(), lat, lon, correctAlt); // @Types の型定義と SunCalc の実装が異なるので、この行のみ型検査無効
+  const sunriseTime = times.sunrise.toLocaleTimeString();
+  const sunsetTime = times.sunset.toLocaleTimeString();
 
   return { sunriseTime, sunsetTime };
 };
