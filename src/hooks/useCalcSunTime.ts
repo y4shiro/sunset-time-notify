d3f6785069bb315e7aa57a08bca1e373de1a2678
y4shiro <y4shironao@gmail.com>
2022-04-07 23:31:05 +0900

    useCalcSunTime 関数の拡張性とテスタビリティを向上させるため、引数に date を追加

--- src/hooks/useCalcSunTime.ts
+++ src/hooks/useCalcSunTime.ts
@@ -1,9 +1,9 @@
 import SunCalc from 'suncalc';
 import format from 'date-fns/format';
 
-export const useCalcSunTime = (lat: number, lon: number, alt: number) => {
+export const useCalcSunTime = (lat: number, lon: number, alt: number, date: Date = new Date()) => {
   const correctAlt = alt < 0 ? 0 : alt; // suncalc は alt(高度) 0m 未満に対応していないので、その場合は 0m として扱う
-  const times = SunCalc.getTimes(new Date(), lat, lon, correctAlt); // @Types の型定義と SunCalc の実装が異なるので、src/types.d.ts で型定義拡張を行った
+  const times = SunCalc.getTimes(date, lat, lon, correctAlt); // @Types の型定義と SunCalc の実装が異なるので、src/types.d.ts で型定義拡張を行った
 
   const sunriseTime = format(times.sunrise, 'HH:mm:ss');
   const sunsetTime = format(times.sunset, 'HH:mm:ss');
