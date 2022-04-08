acc0a19b309d61a24369d6ad28492948c6f0a008
y4shiro <y4shironao@gmail.com>
2022-04-08 19:47:28 +0900

    useCalcSunTime のテストにて、date の値が不正な場合を追加した

--- src/hooks/useCalcSunTime.test.ts
+++ src/hooks/useCalcSunTime.test.ts
@@ -1,12 +1,28 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { cleanup } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import { useCalcSunTime } from './useCalcSunTime';
 
+beforeEach(() => cleanup());
 describe('useCurrentSunTIme', () => {
-  test('特定の座標と日付を渡すと、正しい日の出日の入時刻の文字列を返す', () => {
+  test('座標と日付を渡すと、それに対応した日の出日の入時刻の文字列を返す', () => {
     const { result } = renderHook(() =>
       useCalcSunTime(35.659241, 139.701144, 0, new Date('2022-01-01T12:00:00')),
     );
     expect(result.current.sunriseTime).toBe('06:52:02');
     expect(result.current.sunsetTime).toBe('16:39:13');
+  });
+
+  test('引数で渡ってきた date の値が "Invalid Date" の場合、"不正な日付です" という文字列を返す', () => {
+    const { result } = renderHook(() =>
+      useCalcSunTime(35.659241, 139.701144, 0, new Date('2022-01-00T12:00:00')),
+    );
+    expect(result.current.sunriseTime).toBe('不正な日付です');
+    expect(result.current.sunsetTime).toBe('不正な日付です');
+  });
+
+  test('SunCalc で算出した date の値が "Invalid Date" の場合、"白夜または極夜です" という文字列を返す', () => {
+    const { result } = renderHook(() => useCalcSunTime(80, 0, 0, new Date('2022-01-01T12:00:00')));
+    expect(result.current.sunriseTime).toBe('白夜または極夜です');
+    expect(result.current.sunsetTime).toBe('白夜または極夜です');
   });
 });
