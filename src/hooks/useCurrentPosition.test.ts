61de8f1f04c7e0b7996b6bd5844abdfa5e5caaad
y4shiro <y4shironao@gmail.com>
2022-04-12 19:21:45 +0900

    useCurrentPosition のテストにて、座標と高さが取得出来た場合を追加

--- src/hooks/useCurrentPosition.test.ts
+++ src/hooks/useCurrentPosition.test.ts
@@ -2,14 +2,14 @@ import { cleanup } from '@testing-library/react';
 import { renderHook, act } from '@testing-library/react-hooks';
 import { useCurrentPosition } from './useCurrentPosition';
 
-const geolocationPosition = {
+const geolocationPosition: GeolocationPosition = {
   coords: {
     accuracy: 1,
-    altitude: null,
+    altitude: 10,
     altitudeAccuracy: null,
     heading: null,
-    latitude: 1,
-    longitude: 1,
+    latitude: 35.681974167122895,
+    longitude: 139.76716432155922,
     speed: null,
   },
   timestamp: 1,
@@ -37,16 +37,22 @@ describe('navigator が存在する場合', () => {
   afterEach(() => {
     navigatorSpy?.mockRestore();
   });
-  test('useState の初期値がそれぞれ正しいことを確認', async () => {
-    const { result } = renderHook(() => useCurrentPosition());
+  test('座標と高さが取得出来る場合、各値が useState に格納されている', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useCurrentPosition());
 
-    await act(async () => {
-      expect(result.current.lat).toBe(0);
-      expect(result.current.lon).toBe(0);
-      expect(result.current.alt).toBe(0);
-      expect(result.current.locationError).toBe('');
-      expect(result.current.isLoading).toBeTruthy();
-    });
+    expect(result.current.lat).toBe(0);
+    expect(result.current.lon).toBe(0);
+    expect(result.current.alt).toBe(0);
+    expect(result.current.locationError).toBe('');
+    expect(result.current.isLoading).toBeTruthy();
+
+    await waitForNextUpdate();
+
+    expect(result.current.lat).toBe(35.681974167122895);
+    expect(result.current.lon).toBe(139.76716432155922);
+    expect(result.current.alt).toBe(10);
+    expect(result.current.locationError).toBe('');
+    expect(result.current.isLoading).toBeFalsy();
   });
 });
 
