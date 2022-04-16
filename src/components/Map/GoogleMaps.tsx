b3359bcae4014c0626aabd56ee5c778f688e79f4
y4shiro <y4shironao@gmail.com>
2022-04-16 22:11:48 +0900

    google-map-react インストール

---
+++ src/components/Map/GoogleMaps.tsx
@@ -0,0 +1,12 @@
+import React, { VFC } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const GoogleMaps: VFC = () => {
+  return (
+    <div>
+      <GoogleMapReact></GoogleMapReact>
+    </div>
+  );
+};
+
+export default GoogleMaps;
