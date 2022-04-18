1529b2798ffedd627cbce6e2a877a57f16b9040d
y4shiro <y4shironao@gmail.com>
2022-04-18 10:11:31 +0900

    Layout の挙動を調査するため layout-test ページを追加

---
+++ src/pages/layout-test.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+import LayoutTest from '../components/LayoutTest';
+
+const LayoutTestContent: NextPage = () => {
+  return <LayoutTest />;
+};
+
+export default LayoutTestContent;
