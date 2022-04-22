397b527fc089c207f6071a31eaf6b4ff9a2aff7a
y4shiro <y4shironao@gmail.com>
2022-04-22 23:11:29 +0900

    不要なページとコンポーネント削除

--- src/pages/layout-test.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next';
-
-import LayoutTest from '../components/LayoutTest';
-
-const LayoutTestContent: NextPage = () => {
-  return <LayoutTest />;
-};
-
-export default LayoutTestContent;
