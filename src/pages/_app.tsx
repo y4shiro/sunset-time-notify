4d17e6372d0caac9a4b45cf8a198d259ff13d255
y4shiro <y4shironao@gmail.com>
2022-04-25 17:42:10 +0900

    不要な行を削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider>
-        {' '}
         <Layout>
           <Component {...pageProps} />
         </Layout>
