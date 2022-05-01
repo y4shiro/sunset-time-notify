dd52c4b1abed42770bd736fd5d3d309bf2afcf1b
y4shiro <y4shironao@gmail.com>
2022-05-01 23:21:22 +0900

    日の出日の入時刻部分のパディング調整

--- src/components/SunsetInfo/SunTime.tsx
+++ src/components/SunsetInfo/SunTime.tsx
@@ -10,12 +10,12 @@ export const SunTime: VFC = () => {
 
   return (
     <HStack w='full' justifyContent='space-around'>
-      <Box p='4' borderRadius='xl' shadow='lg' bgColor='#FFBF15'>
+      <Box p={{ base: 4, md: 6 }} borderRadius='xl' shadow='lg' bgColor='#FFBF15'>
         <Text textColor='white' fontSize={{ base: 20, md: 24 }} fontWeight='bold'>
           日の出 {isLoading ? '読込中' : `${sunriseTime}`}
         </Text>
       </Box>
-      <Box p='4' borderRadius='xl' shadow='lg' bgColor='#142C8C'>
+      <Box p={{ base: 4, md: 6 }} borderRadius='xl' shadow='lg' bgColor='#142C8C'>
         <Text textColor='white' fontSize={{ base: 20, md: 24 }} fontWeight='bold'>
           日の入 {isLoading ? '読込中' : `${sunsetTime}`}
         </Text>
