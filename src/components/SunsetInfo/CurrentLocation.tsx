7f277a44e5e04cdd08a740b4d67f9a9177d84a97
y4shiro <y4shironao@gmail.com>
2022-05-02 19:46:47 +0900

    座標部分にアイコン追加

--- src/components/SunsetInfo/CurrentLocation.tsx
+++ src/components/SunsetInfo/CurrentLocation.tsx
@@ -1,5 +1,6 @@
 import { VFC } from 'react';
-import { Box, HStack, Text } from '@chakra-ui/react';
+import { HStack, Text } from '@chakra-ui/react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 import { useCurrentPosition } from '../../hooks/useCurrentPosition';
 
@@ -7,7 +8,7 @@ export const CurrentLocation: VFC = () => {
   const { latitude, longitude, altitude, locationError } = useCurrentPosition();
 
   return (
-    <Box
+    <HStack
       w='full'
       p={4}
       borderRadius='lg'
@@ -16,20 +17,19 @@ export const CurrentLocation: VFC = () => {
       shadow='md'
       bgColor='white'
     >
-      <HStack>
-        {locationError ? (
-          <Text fontSize='20'>位置情報の取得に失敗しました</Text>
-        ) : (
-          <>
-            {/** 緯度経度が 0 度の場合は取得中と表示する。レアケースだが、緯度経度 0 の地点は表示できないので、この辺厳密にする場合は取得中ステータスで管理する*/}
-            <Text fontSize={{ base: 16, md: 20 }}>緯度:{latitude.toFixed(4) || '取得中'}</Text>{' '}
-            <Text fontSize={{ base: 16, md: 20 }}>経度:{longitude.toFixed(4) || '取得中'}</Text>{' '}
-            <Text fontSize={{ base: 16, md: 20 }}>
-              高度:{altitude ? altitude.toFixed(4) : '未取得'}
-            </Text>
-          </>
-        )}
-      </HStack>
-    </Box>
+      <FaMapMarkerAlt fontSize='24' />
+      {locationError ? (
+        <Text fontSize='20'>位置情報の取得に失敗しました</Text>
+      ) : (
+        <>
+          {/** 緯度経度が 0 度の場合は取得中と表示する。レアケースだが、緯度経度 0 の地点は表示できないので、この辺厳密にする場合は取得中ステータスで管理する*/}
+          <Text fontSize={{ base: 16, md: 20 }}>緯度:{latitude.toFixed(4) || '取得中'}</Text>{' '}
+          <Text fontSize={{ base: 16, md: 20 }}>経度:{longitude.toFixed(4) || '取得中'}</Text>{' '}
+          <Text fontSize={{ base: 16, md: 20 }}>
+            高度:{altitude ? altitude.toFixed(4) : '未取得'}
+          </Text>
+        </>
+      )}
+    </HStack>
   );
 };
