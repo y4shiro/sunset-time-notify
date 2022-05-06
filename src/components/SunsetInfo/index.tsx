77dae3fafa804f3ca2e06841f6d0d32d6b6258ba
y4shiro <y4shironao@gmail.com>
2022-05-06 19:20:26 +0900

    SunsetInfo の width の最大値/最小値を設定

--- src/components/SunsetInfo/index.tsx
+++ src/components/SunsetInfo/index.tsx
@@ -7,7 +7,15 @@ import { SunTime } from './SunTime';
 
 const SunsetInfo: VFC = () => {
   return (
-    <VStack w='full' gap='2' p={{ base: 4, md: 6 }} shadow='lg' bgColor='gray.100'>
+    <VStack
+      w={{ base: 'full' }}
+      minW={{ md: '400px' }}
+      maxW={{ md: '540px' }}
+      gap='2'
+      p={{ base: 4, md: 6 }}
+      shadow='lg'
+      bgColor='gray.100'
+    >
       <CurrentLocation />
       <CurrentTime />
       <SunTime />
