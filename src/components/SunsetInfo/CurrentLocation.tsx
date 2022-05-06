488d7787e6575488d332331e35bc3670e8bef025
y4shiro <y4shironao@gmail.com>
2022-05-06 15:43:59 +0900

    座標部分のスタイルを修正し視認性を高めた

--- src/components/SunsetInfo/CurrentLocation.tsx
+++ src/components/SunsetInfo/CurrentLocation.tsx
@@ -1,5 +1,5 @@
 import { VFC } from 'react';
-import { HStack, Text } from '@chakra-ui/react';
+import { Box, Divider, HStack, SimpleGrid, Text, TextProps, VStack } from '@chakra-ui/react';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 
 import { useCurrentPosition } from '../../hooks/useCurrentPosition';
@@ -10,12 +10,14 @@ export const CurrentLocation: VFC = () => {
   return (
     <HStack
       w='full'
-      p={4}
+      p='4'
+      textAlign='center'
       borderRadius='lg'
       border='1px'
       borderColor='gray.200'
       shadow='md'
       bgColor='white'
+      justify='space-around'
     >
       <FaMapMarkerAlt fontSize='24' />
       {locationError ? (
@@ -23,13 +25,29 @@ export const CurrentLocation: VFC = () => {
       ) : (
         <>
           {/** 緯度経度が 0 度の場合は取得中と表示する。レアケースだが、緯度経度 0 の地点は表示できないので、この辺厳密にする場合は取得中ステータスで管理する*/}
-          <Text fontSize={{ base: 16, md: 20 }}>緯度:{latitude.toFixed(4) || '取得中'}</Text>{' '}
-          <Text fontSize={{ base: 16, md: 20 }}>経度:{longitude.toFixed(4) || '取得中'}</Text>{' '}
-          <Text fontSize={{ base: 16, md: 20 }}>
-            高度:{altitude ? altitude.toFixed(4) : '未取得'}
-          </Text>
+          <Box>
+            <TitleText>緯度</TitleText>
+            <ContentText>{latitude.toFixed(4) || '取得中'}</ContentText>
+          </Box>
+          <Divider height='40px' orientation='vertical' borderColor='gray.300' />
+
+          <Box>
+            <TitleText>経度</TitleText>
+            <ContentText>{longitude.toFixed(4) || '取得中'}</ContentText>
+          </Box>
+          <Divider height='40px' orientation='vertical' borderColor='gray.300' />
+
+          <Box>
+            <TitleText>高度</TitleText>
+            <ContentText>{altitude ? altitude.toFixed(4) : '未取得'}</ContentText>
+          </Box>
         </>
       )}
     </HStack>
   );
 };
+
+const TitleText: VFC<TextProps> = (props) => (
+  <Text fontSize={{ base: 12, md: 14 }} textColor='gray.400' {...props} />
+);
+const ContentText: VFC<TextProps> = (props) => <Text fontSize={{ base: 16, md: 20 }} {...props} />;
