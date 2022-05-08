ee1b2bc15598191c237a93aab7af743afade31f4
y4shiro <y4shironao@gmail.com>
2022-05-08 15:37:22 +0900

    canonical の typo 修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       <NextHeadSeo
         title='Sunset Time Notify'
         description='任意の日時 / 地点の日没時刻を表示したり、Push 通知などで日没が近づくと通知する Web サービス'
-        canonical='https/sunset-time-notify.y4shiro.net/'
+        canonical='https://sunset-time-notify.y4shiro.net/'
       />
       <MainContent />
     </>
