0e0228bd77d48e59018116556ac64901282da105
y4shiro <y4shironao@gmail.com>
2022-05-08 15:32:17 +0900

    catnose99/next-head-seo を導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from 'next';
+import NextHeadSeo from 'next-head-seo';
 
 import MainContent from '../components/MainContent';
 
 const Home: NextPage = () => {
-  return <MainContent />;
+  return (
+    <>
+      <NextHeadSeo
+        title='Sunset Time Notify'
+        description='任意の日時 / 地点の日没時刻を表示したり、Push 通知などで日没が近づくと通知する Web サービス'
+        canonical='https/sunset-time-notify.y4shiro.net/'
+      />
+      <MainContent />
+    </>
+  );
 };
 
 export default Home;
