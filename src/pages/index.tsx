27b0648db287f85850cbed5d65c72fc922974027
y4shiro <y4shironao@gmail.com>
2022-05-09 17:03:21 +0900

    OGP 関連を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,18 @@ import NextHeadSeo from 'next-head-seo';
 import MainContent from '../components/MainContent';
 
 const Home: NextPage = () => {
+  const title = 'Sunset Time Notify';
+  const description =
+    '任意の日時 / 地点の日没時刻を表示したり、Push 通知などで日没が近づくと通知する Web サービス';
+  const url = 'https://sunset-time-notify.y4shiro.net/';
+
   return (
     <>
       <NextHeadSeo
-        title='Sunset Time Notify'
-        description='任意の日時 / 地点の日没時刻を表示したり、Push 通知などで日没が近づくと通知する Web サービス'
-        canonical='https://sunset-time-notify.y4shiro.net/'
+        title={title}
+        description={description}
+        canonical={url}
+        og={{ title, description, url, siteName: title, type: 'website' }}
       />
       <MainContent />
     </>
