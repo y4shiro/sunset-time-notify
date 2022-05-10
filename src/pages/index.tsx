931d458fb31327cca31f7528bf146ea9e8969938
y4shiro <y4shironao@gmail.com>
2022-05-10 11:36:17 +0900

    不要な console.log 削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ const Home: NextPage = () => {
     '任意の日時 / 地点の日没時刻を表示したり、Push 通知などで日没が近づくと通知する Web サービス';
   const url = 'https://sunset-time-notify.y4shiro.net/';
   const ogpImgPath = 'sunset.png';
-  console.log(`${url}${ogpImgPath}`);
 
   return (
     <>
