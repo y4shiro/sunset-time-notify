03b6a3bf7b6919acd72f4821e1d59e6e8905ca91
y4shiro <y4shironao@gmail.com>
2022-05-11 15:39:43 +0900

    日の出日の入までの残り時間を表示するコンポーネント RemainingTime を追加

--- src/components/SunsetInfo/RemainingTime.tsx
+++ src/components/SunsetInfo/RemainingTime.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { useState, VFC } from 'react';
+import { Text, HStack } from '@chakra-ui/react';
+import { FaClock } from 'react-icons/fa';
 
-const RemainingTime = () => {
-  return <div>RemainingTime</div>;
+type isTime = 'day' | 'night';
+
+const RemainingTime: VFC = () => {
+  const [isTime, setIsTime] = useState<isTime>('day');
+
+  return (
+    <HStack
+      w='full'
+      p='4'
+      gap='2'
+      borderRadius='lg'
+      border='1px'
+      borderColor='gray.200'
+      shadow='md'
+      bgColor='white'
+    >
+      <FaClock fontSize='24' />
+      <Text fontSize={{ base: 16, md: 20 }}>
+        {isTime === 'day' ? '日の入' : '日の出'}まであと 00:00:00
+      </Text>
+    </HStack>
+  );
 };
 
-export default RemainingTime;
+export { RemainingTime };
