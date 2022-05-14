1f7f4a617aed9caf380883d1ec73ecf6b8dfba72
y4shiro <y4shironao@gmail.com>
2022-05-14 23:00:23 +0900

    日の出日の入時刻の計算関数を useCalcSuntime.ts として分割

--- src/hooks/useCurrentSuntime.ts
+++ src/hooks/useCurrentSuntime.ts
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
-import SunCalc from 'suncalc';
 import { format, isValid } from 'date-fns';
 
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { altitudeState, latitudeState, longitudeState } from '../stores/currentPositionState';
 import { sunriseTimeState, sunsetTimeState, suntimeIsvalidState } from '../stores/sunTimeState';
+
+import { useCalcSuntime } from './useCalcSuntime';
 
 export const useCurrentSuntime = (
   date: Date = new Date(),
@@ -16,6 +17,8 @@ export const useCurrentSuntime = (
   const [sunsetTime, setSunsetTime] = useRecoilState(sunsetTimeState);
   const [suntimeIsvalid, setSuntimeIsvalid] = useRecoilState(suntimeIsvalidState);
 
+  const { sunrise, sunset } = useCalcSuntime(date, latitude, longitude, altitude);
+
   useEffect(() => {
     // 引数で渡ってきた date が Invalid Date の場合
     if (!isValid(date)) {
@@ -25,12 +28,9 @@ export const useCurrentSuntime = (
       return;
     }
 
-    const correctAlt = altitude < 0 ? 0 : altitude; // suncalc は alt(高度) 0m 未満に対応していないので、その場合は 0m として扱う
-    const times = SunCalc.getTimes(date, latitude, longitude, correctAlt); // @Types の型定義と SunCalc の実装が異なるので、src/types.d.ts で型定義拡張を行った
-
     // SunCalc で算出した日時が Invalide Date だった場合
     // 基本的に白夜・極夜の場合が多いが、他のパターンが漏れている可能性もある
-    if (!isValid(times.sunrise)) {
+    if (!isValid(sunrise)) {
       setSunriseTime('白夜または極夜');
       setSunsetTime('白夜または極夜');
       setSuntimeIsvalid(false);
@@ -38,8 +38,8 @@ export const useCurrentSuntime = (
     }
 
     // 正常に日時が算出できた場合は、日の出日の入時刻を文字列で返す
-    setSunriseTime(format(times.sunrise, 'HH:mm:ss'));
-    setSunsetTime(format(times.sunset, 'HH:mm:ss'));
+    setSunriseTime(format(sunrise, 'HH:mm:ss'));
+    setSunsetTime(format(sunset, 'HH:mm:ss'));
     setSuntimeIsvalid(true);
   }, [date, latitude, longitude, altitude]);
 
