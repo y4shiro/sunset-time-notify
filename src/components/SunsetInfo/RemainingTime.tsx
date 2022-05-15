376c4e0103b3fdbfe03bafc1b49216a139a5cc69
y4shiro <y4shironao@gmail.com>
2022-05-15 21:01:06 +0900

    日の出日の入時刻までの残り時間を返すカスタムフック useRemainingTime を実装

--- src/components/SunsetInfo/RemainingTime.tsx
+++ src/components/SunsetInfo/RemainingTime.tsx
@@ -1,11 +1,11 @@
-import { useState, VFC } from 'react';
+import { VFC } from 'react';
 import { Text, HStack } from '@chakra-ui/react';
 import { FaClock } from 'react-icons/fa';
 
-type isTime = 'day' | 'night';
+import { useRemainingTime } from '../../hooks/useRemainingTime';
 
 const RemainingTime: VFC = () => {
-  const [isTime, setIsTime] = useState<isTime>('day');
+  const { remainingTime, isTime } = useRemainingTime();
 
   return (
     <HStack
@@ -20,7 +20,7 @@ const RemainingTime: VFC = () => {
     >
       <FaClock fontSize='24' />
       <Text fontSize={{ base: 16, md: 20 }}>
-        {isTime === 'day' ? '日の入' : '日の出'}まであと 00:00:00
+        {isTime === 'beforeSunset' ? '日の入' : '日の出'}まであと {remainingTime}
       </Text>
     </HStack>
   );
---
+++ src/hooks/useRemainingTime.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { intervalToDuration, isValid, nextDay } from 'date-fns';
+
+import { useRecoilValue } from 'recoil';
+import { altitudeState, latitudeState, longitudeState } from '../stores/currentPositionState';
+import { suntimeIsvalidState } from '../stores/sunTimeState';
+
+import { useCalcSuntime } from './useCalcSuntime';
+import { useCurrentTime } from './useCurrentTime';
+
+type isTime = 'beforeSunrise' | 'beforeSunset' | 'nextSunrise';
+
+export const useRemainingTime = (): { remainingTime: string; isTime: isTime } => {
+  const latitude = useRecoilValue(latitudeState); // Latitude 緯度
+  const longitude = useRecoilValue(longitudeState); // Longitude 経度
+  const altitude = useRecoilValue(altitudeState); // Altitude 高度
+
+  const today = new Date();
+  const { currentTime } = useCurrentTime();
+
+  const [remainingTime, setRemainingTime] = useState<string>('');
+  const [isTime, setIsTime] = useState<isTime>('beforeSunrise');
+
+  const { sunrise, sunset } = useCalcSuntime(today, latitude, longitude, altitude);
+  const { sunrise: nextSunrise } = useCalcSuntime(nextDay(today, 1), latitude, longitude, altitude);
+
+  const formatStringTime = (remain: Duration) => {
+    const hours = String(remain.hours).padStart(2, '0');
+    const minutes = String(remain.minutes).padStart(2, '0');
+    const seconds = String(remain.seconds).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  useEffect(() => {
+    if (currentTime < sunrise) {
+      const remain = intervalToDuration({ start: currentTime, end: sunrise });
+      setRemainingTime(formatStringTime(remain));
+      setIsTime('beforeSunrise');
+    } else if (sunrise < currentTime && currentTime < sunset) {
+      const remain = intervalToDuration({ start: currentTime, end: sunset });
+      setRemainingTime(formatStringTime(remain));
+      setIsTime('beforeSunset');
+    } else if (sunset < currentTime && currentTime < nextSunrise) {
+      const remain = intervalToDuration({ start: currentTime, end: nextSunrise });
+      setRemainingTime(formatStringTime(remain));
+      setIsTime('nextSunrise');
+    }
+  }, [currentTime]);
+
+  return { remainingTime, isTime };
+};
