ba1ee8dd41ba69bf0b1cc84f51ffe93434e2f66c
y4shiro <y4shironao@gmail.com>
2022-05-24 18:19:59 +0900

    Yahoo の API 関連コードを削除、axios もアンインストール

--- src/components/SunsetInfo/ReverseGeocoding.tsx
+++ src/components/SunsetInfo/ReverseGeocoding.tsx
@@ -1,7 +1,4 @@
 import React, { useState, VFC } from 'react';
-import axios from 'axios';
-// @ts-ignore
-import axiosJsonpAdapter from 'axios-jsonp';
 
 import { Button, HStack, Text } from '@chakra-ui/react';
 import { FaClock } from 'react-icons/fa';
@@ -13,7 +10,6 @@ const ReverseGeocoding: VFC = () => {
   const { latitude, longitude } = useCurrentPosition();
 
   const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY || '';
-  const yahooClientID = process.env.NEXT_PUBLIC_YAHOO_CLIENT_ID || '';
 
   const getPlaceName = () => {
     const fetchUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxAccessToken}&language=ja&types=country,region,locality,place`;
@@ -23,30 +19,6 @@ const ReverseGeocoding: VFC = () => {
         console.log(data);
         const address = data.features[0].place_name;
         setPlaceName(address);
-      });
-  };
-
-  const getPlaceNameYahoo = () => {
-    const fetchUrl = `https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?`;
-    axios
-      .get(fetchUrl, {
-        adapter: axiosJsonpAdapter,
-        params: {
-          appid: yahooClientID,
-          lat: latitude,
-          lon: longitude,
-          output: 'json',
-        },
-      })
-      .then((res) => {
-        const data = res.data.Feature[0].Property;
-        console.log(data);
-        const country = data.Country['Name'];
-        const address = data.Address;
-        setPlaceName(`${country}, ${address}`);
-      })
-      .catch((err) => {
-        console.log(err.message);
       });
   };
 
@@ -66,7 +38,6 @@ const ReverseGeocoding: VFC = () => {
         <Text fontSize={{ base: 16, md: 20 }}>{placeName}</Text>
         <HStack>
           <Button onClick={getPlaceName}>Mapbox</Button>
-          <Button onClick={getPlaceNameYahoo}>Yahoo</Button>
         </HStack>
       </HStack>
     </HStack>
