8408312f0be7cd6b6d5a71760fee6550d2031e39
y4shiro <y4shironao@gmail.com>
2022-05-25 16:08:22 +0900

    React Query インストール

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,34 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Layout from '../components/Layout';
 import { theme } from '../utils/theme';
 import GoogleAnalytics from '../components/GoogleAnalytics';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      staleTime: 300_000,
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ChakraProvider theme={theme}>
-        {process.env.NODE_ENV === 'production' && <GoogleAnalytics />}
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ChakraProvider theme={theme}>
+          {process.env.NODE_ENV === 'production' && <GoogleAnalytics />}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
