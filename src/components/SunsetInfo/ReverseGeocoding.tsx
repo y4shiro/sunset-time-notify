ba1a8e998881a53985620d057fbca5bfe2375dc0
y4shiro <y4shironao@gmail.com>
2022-05-25 16:25:21 +0900

    Geocoding API へのリクエストとキャッシュ管理を React Query で行うように変更

--- src/components/SunsetInfo/ReverseGeocoding.tsx
+++ src/components/SunsetInfo/ReverseGeocoding.tsx
@@ -1,26 +1,24 @@
-import React, { useState, VFC } from 'react';
+import React, { VFC } from 'react';
+import { useQuery } from 'react-query';
 
 import { Button, HStack, Text } from '@chakra-ui/react';
 import { FaClock } from 'react-icons/fa';
 
 import { useCurrentPosition } from '../../hooks/useCurrentPosition';
 
+const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY || '';
+
 const ReverseGeocoding: VFC = () => {
-  const [placeName, setPlaceName] = useState('');
   const { latitude, longitude } = useCurrentPosition();
 
-  const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY || '';
-
-  const getPlaceName = () => {
+  const fetchPlaceName = async () => {
     const fetchUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxAccessToken}&language=ja&types=country,region,locality,place`;
-    fetch(fetchUrl, { method: 'GET' })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        const address = data.features[0].place_name;
-        setPlaceName(address);
-      });
+    const data = await (await fetch(fetchUrl)).json();
+    const placeName = await data.features[0].place_name;
+    return placeName;
   };
+
+  const { data: placeName, isLoading, isFetching, refetch } = useQuery('placeName', fetchPlaceName);
 
   return (
     <HStack
@@ -35,10 +33,10 @@ const ReverseGeocoding: VFC = () => {
     >
       <FaClock fontSize='24' />
       <HStack w='full' bgColor='blue.100' justify='space-between'>
-        <Text fontSize={{ base: 16, md: 20 }}>{placeName}</Text>
-        <HStack>
-          <Button onClick={getPlaceName}>Mapbox</Button>
-        </HStack>
+        <Text fontSize={{ base: 16, md: 20 }}>
+          {isLoading || isFetching ? 'Loading...' : placeName}
+        </Text>
+        <Button onClick={() => refetch()}>再読み込み</Button>
       </HStack>
     </HStack>
   );
