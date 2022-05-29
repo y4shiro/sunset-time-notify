2a0cb6cd470d890b7e2f2cc16d61a99441ea523e
y4shiro <y4shironao@gmail.com>
2022-05-29 10:24:47 +0900

    緯度経度標高部分にて、数字の桁数増減によって横幅にブレが生じる現象を修正

--- src/components/SunsetInfo/CurrentLocation.tsx
+++ src/components/SunsetInfo/CurrentLocation.tsx
@@ -27,19 +27,19 @@ export const CurrentLocation: VFC = () => {
         <Divider />
         <HStack w='full' textAlign='center' justify='space-evenly'>
           {/** 緯度経度が 0 度の場合は取得中と表示する。レアケースだが、緯度経度 0 の地点は表示できないので、この辺厳密にする場合は取得中ステータスで管理する*/}
-          <Box>
+          <Box w='33%'>
             <TitleText>緯度</TitleText>
             <ContentText>{latitude.toFixed(4) || '取得中'}</ContentText>
           </Box>
           <Divider height='40px' orientation='vertical' borderColor='gray.300' />
 
-          <Box>
+          <Box w='33%'>
             <TitleText>経度</TitleText>
             <ContentText>{longitude.toFixed(4) || '取得中'}</ContentText>
           </Box>
           <Divider height='40px' orientation='vertical' borderColor='gray.300' />
 
-          <Box>
+          <Box w='33%'>
             <TitleText>高度</TitleText>
             <ContentText>{altitude ? altitude.toFixed(4) : '未取得'}</ContentText>
           </Box>
