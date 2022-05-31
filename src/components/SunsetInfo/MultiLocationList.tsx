2701ce0c3e51a3209ace8086faa04bdef99c2c2d
y4shiro <y4shironao@gmail.com>
2022-05-31 17:55:22 +0900

    LocationItem の並びを調整

--- src/components/SunsetInfo/MultiLocationList.tsx
+++ src/components/SunsetInfo/MultiLocationList.tsx
@@ -1,32 +1,82 @@
 import React, { VFC } from 'react';
-import { Box, HStack, Text, VStack } from '@chakra-ui/react';
+import { StackDivider, HStack, Text, VStack } from '@chakra-ui/react';
+
+type locationType = {
+  id: number;
+  location: {
+    latitude: number;
+    longitude: number;
+    altitude: number;
+    name: string;
+  };
+  enabledNotify: boolean;
+};
+
+const locationData: locationType[] = [
+  {
+    id: 1,
+    location: {
+      latitude: 35.6814,
+      longitude: 139.7671,
+      altitude: 14.5352,
+      name: '日本, 東京都千代田区',
+    },
+    enabledNotify: false,
+  },
+  {
+    id: 2,
+    location: {
+      latitude: 35.6909,
+      longitude: 139.7003,
+      altitude: 52.27,
+      name: '日本, 東京都新宿区',
+    },
+    enabledNotify: false,
+  },
+  {
+    id: 3,
+    location: {
+      latitude: 35.466,
+      longitude: 139.6227,
+      altitude: 9.198,
+      name: '日本, 神奈川県横浜市西区',
+    },
+    enabledNotify: false,
+  },
+];
 
 const MultiLocationList: VFC = () => {
   return (
     <VStack
       w='full'
-      p={{ base: 3, md: 4 }}
-      gap='2'
+      p='2'
       borderRadius='lg'
       border='1px'
       borderColor='gray.200'
       shadow='md'
       bgColor='white'
+      divider={<StackDivider />}
     >
-      <LocationItem name='地名1' />
-      <LocationItem name='地名2' />
-      <LocationItem name='地名3' />
+      {locationData.map((data) => (
+        <LocationItem key={data.id} {...data} />
+      ))}
     </VStack>
   );
 };
 
-const LocationItem = (props: { name: string }) => {
+const LocationItem = (props: locationType) => {
+  const { latitude, longitude, altitude, name } = props.location;
+
   return (
     <HStack w='full' p='2' bgColor='blue.100'>
-      <Text>{props.name}</Text>
-      <Text>緯度: 0000</Text>
-      <Text>経度: 0000</Text>
-      <Text>高度: 0000</Text>
+      <VStack w='full'>
+        <Text w='full'>{name}</Text>
+        <HStack w='full'>
+          <Text>緯度: {latitude}</Text>
+          <Text>経度: {longitude}</Text>
+          <Text>高度: {altitude}</Text>
+        </HStack>
+      </VStack>
     </HStack>
   );
 };
