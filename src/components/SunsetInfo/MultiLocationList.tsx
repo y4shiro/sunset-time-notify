bb71b33042a5095cfd8450bfeb2c58b5696a018c
y4shiro <y4shironao@gmail.com>
2022-05-31 18:54:31 +0900

    LocationItem に日の出日の入時刻の項目を追加

--- src/components/SunsetInfo/MultiLocationList.tsx
+++ src/components/SunsetInfo/MultiLocationList.tsx
@@ -1,6 +1,9 @@
 import React, { VFC } from 'react';
+import { calcSuntime } from '../../lib/calcSuntime';
+
 import { StackDivider, HStack, Text, VStack, IconButton } from '@chakra-ui/react';
 import { BiTrash } from 'react-icons/bi';
+import { format } from 'date-fns';
 
 type locationType = {
   id: number;
@@ -27,20 +30,20 @@ const locationData: locationType[] = [
   {
     id: 2,
     location: {
-      latitude: 35.6909,
-      longitude: 139.7003,
-      altitude: 52.27,
-      name: '日本, 東京都新宿区',
+      latitude: 34.7017,
+      longitude: 135.4982,
+      altitude: 17.7244,
+      name: '日本, 大阪府大阪市北区',
     },
     enabledNotify: false,
   },
   {
     id: 3,
     location: {
-      latitude: 35.466,
-      longitude: 139.6227,
-      altitude: 9.198,
-      name: '日本, 神奈川県横浜市西区',
+      latitude: 33.59,
+      longitude: 130.4236,
+      altitude: 9.9134,
+      name: '日本, 福岡県福岡市博多区',
     },
     enabledNotify: false,
   },
@@ -67,6 +70,10 @@ const MultiLocationList: VFC = () => {
 
 const LocationItem = (props: locationType) => {
   const { latitude, longitude, altitude, name } = props.location;
+  const { sunrise, sunset } = calcSuntime(new Date(), latitude, longitude, altitude);
+
+  const formattedSunrise = format(sunrise, 'HH:mm:ss');
+  const formattedSunset = format(sunset, 'HH:mm:ss');
 
   return (
     <HStack w='full' px='2' justify='space-between' bgColor='blue.100'>
@@ -76,6 +83,10 @@ const LocationItem = (props: locationType) => {
           <Text>緯度: {latitude}</Text>
           <Text>経度: {longitude}</Text>
           <Text>高度: {altitude}</Text>
+        </HStack>
+        <HStack w='full'>
+          <Text>日の出: {formattedSunrise}</Text>
+          <Text>日の入: {formattedSunset}</Text>
         </HStack>
       </VStack>
       <IconButton color='red' aria-label='TrashButton' icon={<BiTrash size='24' />}>
