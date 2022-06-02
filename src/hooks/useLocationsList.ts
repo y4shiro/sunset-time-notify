626ffd6c9f6cb1ec481fca8e826d9d1db2f4abad
y4shiro <y4shironao@gmail.com>
2022-06-02 22:36:54 +0900

    LocationsList のデータを localStorage を利用して永続化

--- src/hooks/useLocationsList.ts
+++ src/hooks/useLocationsList.ts
@@ -1,45 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { locationType } from '../utils/types';
 import { useAsyncCurrentPosition } from './useAsyncCurrentPosition';
 import { useAsyncCurrentPlaceName } from './useAsyncCurrentPlaceName';
 
-const locationData: locationType[] = [
-  {
-    id: '1',
-    location: {
-      latitude: 35.6814,
-      longitude: 139.7671,
-      altitude: 14.5352,
-      name: '日本, 東京都千代田区',
-    },
-    enabledNotify: false,
-  },
-  {
-    id: '2',
-    location: {
-      latitude: 34.7017,
-      longitude: 135.4982,
-      altitude: 17.7244,
-      name: '日本, 大阪府大阪市北区',
-    },
-    enabledNotify: false,
-  },
-  {
-    id: '3',
-    location: {
-      latitude: 33.59,
-      longitude: 130.4236,
-      altitude: 9.9134,
-      name: '日本, 福岡県福岡市博多区',
-    },
-    enabledNotify: false,
-  },
-];
-
 export const useLocationsList = () => {
-  const [locations, setLocations] = useState<locationType[]>(locationData);
+  const [locations, setLocations] = useState<locationType[]>([]);
   const { getPositionOnce } = useAsyncCurrentPosition();
   const { getCurrentPlaceName } = useAsyncCurrentPlaceName();
 
@@ -66,6 +33,16 @@ export const useLocationsList = () => {
     });
     setLocations(state);
   };
+
+  useEffect(() => {
+    const localData = localStorage.getItem('locations');
+    if (!localData) return;
+    setLocations(JSON.parse(localData));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('locations', JSON.stringify(locations));
+  }, [locations]);
 
   return { locations, removeLocation, addLocation };
 };
