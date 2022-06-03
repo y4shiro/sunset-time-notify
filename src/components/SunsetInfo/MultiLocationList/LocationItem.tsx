4d04b3d79777a072531240e50649991519961d27
y4shiro <y4shironao@gmail.com>
2022-06-03 17:34:45 +0900

    現在地リストのフォントサイズとフォントカラーを変更

--- src/components/SunsetInfo/MultiLocationList/LocationItem.tsx
+++ src/components/SunsetInfo/MultiLocationList/LocationItem.tsx
@@ -1,6 +1,7 @@
-import { HStack, IconButton, Text, VStack } from '@chakra-ui/react';
+import { Box, HStack, IconButton, Text, VStack } from '@chakra-ui/react';
 import { format } from 'date-fns';
 import { BiTrash } from 'react-icons/bi';
+import { FiSunrise, FiSunset } from 'react-icons/fi';
 
 import { calcSuntime } from '../../../lib/calcSuntime';
 import { locationType } from '../../../utils/types';
@@ -14,17 +15,25 @@ export const LocationItem = (props: locationType & { removeLocation: (id: string
   const formattedSunset = format(sunset, 'HH:mm:ss');
 
   return (
-    <HStack w='full' px='2' justify='space-between' bgColor='blue.100'>
-      <VStack w='full'>
-        <Text w='full'>{name}</Text>
-        <HStack w='full'>
+    <HStack w='full' px='2' justify='space-between'>
+      <VStack w='full' spacing='0'>
+        <Text w='full' fontSize='20'>
+          {name}
+        </Text>
+        <HStack w='full' fontSize='14' color='gray.400'>
           <Text>緯度: {latitude.toFixed(4)}</Text>
           <Text>経度: {longitude.toFixed(4)}</Text>
           <Text>高度: {altitude.toFixed(4)}</Text>
         </HStack>
-        <HStack w='full'>
-          <Text>日の出: {formattedSunrise}</Text>
-          <Text>日の入: {formattedSunset}</Text>
+        <HStack w='full' gap='2'>
+          <HStack>
+            <FiSunrise />
+            <Text>{formattedSunrise}</Text>
+          </HStack>
+          <HStack>
+            <FiSunset />
+            <Text>{formattedSunset}</Text>
+          </HStack>
         </HStack>
       </VStack>
       <IconButton
