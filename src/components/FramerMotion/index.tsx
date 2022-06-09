76ab478a6691fdde33ebe084852b08a26c293c65
y4shiro <y4shironao@gmail.com>
2022-06-09 23:46:20 +0900

    Framer Motion animate で keyframe

--- src/components/FramerMotion/index.tsx
+++ src/components/FramerMotion/index.tsx
@@ -15,6 +15,18 @@ const FramerMotion: VFC = () => {
         animate={{ scale: 2 }}
         transition={{ duration: 2 }}
       />
+      <ChakraBox
+        m='24'
+        w='100px'
+        h='100px'
+        bgColor='green'
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+        }}
+        transition={{ duration: 2 }}
+      />
     </Flex>
   );
 };
