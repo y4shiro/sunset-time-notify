4a89a9269c09953cf4399c794dc7f202826d20b4
y4shiro <y4shironao@gmail.com>
2022-06-10 00:12:22 +0900

    Framer Motion drag と MotionValues

--- src/components/FramerMotion/index.tsx
+++ src/components/FramerMotion/index.tsx
@@ -1,43 +1,58 @@
 import { VFC } from 'react';
-import { Box, Flex, BoxProps, ButtonProps, Button } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { Box, Flex, BoxProps, ButtonProps, Button, HStack, Text } from '@chakra-ui/react';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 import type { Variants } from 'framer-motion';
 
-const MotionChakraBox = motion<Omit<BoxProps, 'transition'>>(Box);
+const MotionBox = motion<Omit<BoxProps, 'transition' | 'style'>>(Box);
 const MotionButton = motion<Omit<ButtonProps, 'transition'>>(Button);
 
 const FramerMotion: VFC = () => {
+  const x = useMotionValue(0);
+  const background = useTransform(x, [-100, 0, 100], ['#ff008c', '#7700ff', 'rgb(230, 255, 0)']);
+
   return (
-    <Flex w='100%' minH='80vh' bgColor='gray.100'>
-      <MotionChakraBox
-        m='24'
-        w='100px'
-        h='100px'
-        bgColor='green'
-        animate={{ scale: 2 }}
-        transition={{ duration: 2 }}
-      />
-      <MotionChakraBox
-        m='24'
-        w='100px'
-        h='100px'
-        bgColor='green'
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
-        }}
-        transition={{ duration: 2 }}
-      />
-      <MotionButton
-        m='24'
-        w='100px'
-        h='100px'
-        colorScheme='twitter'
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-      />
-    </Flex>
+    <Box w='100%' minH='80vh' bgColor='gray.100'>
+      <HStack w='100%' gap='32'>
+        <MotionBox
+          m='24'
+          w='100px'
+          h='100px'
+          bgColor='green'
+          animate={{ scale: 2 }}
+          transition={{ duration: 2 }}
+        />
+        <MotionBox
+          m='24'
+          w='100px'
+          h='100px'
+          bgColor='green'
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 270, 270, 0],
+            borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+          }}
+          transition={{ duration: 2 }}
+        />
+        <MotionButton
+          m='24'
+          w='100px'
+          h='100px'
+          colorScheme='twitter'
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        />
+      </HStack>
+
+      <HStack>
+        <MotionBox m='24' w='100px' h='100px' bgColor='twitter' style={{ background }}>
+          <MotionBox drag='x' dragConstraints={{ left: 0, right: 0 }} style={{ x }}>
+            <Text color='gray.200' fontSize='16'>
+              Drag me
+            </Text>
+          </MotionBox>
+        </MotionBox>
+      </HStack>
+    </Box>
   );
 };
 
