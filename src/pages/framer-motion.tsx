f393c33f430766555e04850f8623ca57795c2d30
y4shiro <y4shironao@gmail.com>
2022-06-09 10:35:12 +0900

    Framer Motion 学習用のページ作成

---
+++ src/pages/framer-motion.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+import FramerMotion from '../components/FramerMotion';
+
+const FM: NextPage = () => {
+  return (
+    <>
+      <FramerMotion />
+    </>
+  );
+};
+
+export default FM;
