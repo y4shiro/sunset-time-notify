eb7f0c4b5be36987551f71d1ab54241bb7b98180
y4shiro <y4shironao@gmail.com>
2022-06-14 00:03:55 +0900

    リスト追加削除時に伸縮するアニメーション追加

--- src/components/FramerMotion/index.tsx
+++ src/components/FramerMotion/index.tsx
@@ -1,11 +1,21 @@
 import { useState, VFC } from 'react';
-import { Box, Flex, BoxProps, ButtonProps, Button, HStack, Text, VStack } from '@chakra-ui/react';
-import { motion, useMotionValue, useTransform } from 'framer-motion';
-import type { Variants } from 'framer-motion';
+import {
+  Box,
+  BoxProps,
+  ButtonProps,
+  Button,
+  HStack,
+  StackProps,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
 import { v4 as uuidv4 } from 'uuid';
 
 const MotionBox = motion<Omit<BoxProps, 'transition' | 'style'>>(Box);
 const MotionButton = motion<Omit<ButtonProps, 'transition'>>(Button);
+const MotionHStack = motion<Omit<StackProps, 'transition'>>(HStack);
+const MotionVStack = motion<Omit<StackProps, 'transition'>>(VStack);
 
 const FramerMotion: VFC = () => {
   const [list, setList] = useState<{ id: string }[]>([]);
@@ -15,25 +25,45 @@ const FramerMotion: VFC = () => {
 
   return (
     <Box w='100%' minH='80vh' bgColor='gray.100'>
-      <VStack m='2' gap='2'>
-        <Button colorScheme='green' onClick={() => addList()}>
-          リスト追加
-        </Button>
-        {list.map((item) => (
-          <HStack
-            w='480px'
-            p='2'
-            borderRadius='8'
-            bgColor='twitter.200'
-            justify='space-between'
-            key={item.id}
-          >
-            <Text>{item.id}</Text>
-            <Button onClick={() => removeList(item.id)}>Delete</Button>
-          </HStack>
-        ))}
-      </VStack>
+      <Button colorScheme='green' onClick={() => addList()}>
+        リスト追加
+      </Button>
+      <AnimateSharedLayout>
+        <MotionVStack
+          layout
+          initial={{ borderRadius: 25 }}
+          w='540px'
+          m='2'
+          p='2'
+          bgColor={'red.200'}
+        >
+          <AnimatePresence>
+            {list.map((item) => (
+              <Item key={item.id} id={item.id} removeList={removeList} />
+            ))}
+          </AnimatePresence>
+        </MotionVStack>
+      </AnimateSharedLayout>
     </Box>
+  );
+};
+
+const Item = ({ id, removeList }: { id: string; removeList: (id: string) => void }) => {
+  return (
+    <MotionHStack
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 0.25 } }}
+      exit={{ opacity: 0 }}
+      w='480px'
+      p='2'
+      borderRadius='8'
+      bgColor='twitter.300'
+      justify='space-between'
+    >
+      <Text>{id}</Text>
+      <Button onClick={() => removeList(id)}>Delete</Button>
+    </MotionHStack>
   );
 };
 
