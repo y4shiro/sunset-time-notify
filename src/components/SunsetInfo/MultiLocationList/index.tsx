55cb1d7747d8a8d3278fa69b4e2411b04a264ad5
y4shiro <y4shironao@gmail.com>
2022-06-16 22:15:39 +0900

    現在地リストにフェードアニメーションを追加

--- src/components/SunsetInfo/MultiLocationList/index.tsx
+++ src/components/SunsetInfo/MultiLocationList/index.tsx
@@ -1,29 +1,40 @@
 import { VFC } from 'react';
-import { StackDivider, VStack } from '@chakra-ui/react';
+import { HStack, StackDivider, StackProps, VStack } from '@chakra-ui/react';
 
 import { LocationItem } from './LocationItem';
 import { useLocationsList } from '../../../hooks/useLocationsList';
 import { AddLocationItem } from './AddLocationItem';
+import { AnimatePresence, HTMLMotionProps, LayoutGroup, motion } from 'framer-motion';
+
+type Merge<P, T> = Omit<P, keyof T> & T;
+type MotionStackProps = Merge<StackProps, HTMLMotionProps<'div'>>;
+export const MotionV: React.FC<MotionStackProps> = motion(VStack);
+export const MotionH: React.FC<MotionStackProps> = motion(HStack);
 
 const MultiLocationList: VFC = () => {
   const { locations, removeLocation, addLocation } = useLocationsList();
 
   return (
-    <VStack
-      w='full'
-      p='2'
-      borderRadius='lg'
-      border='1px'
-      borderColor='gray.200'
-      shadow='md'
-      bgColor='white'
-      divider={<StackDivider />}
-    >
-      <AddLocationItem addLocation={addLocation} />
-      {locations.map((data) => (
-        <LocationItem key={data.id} removeLocation={removeLocation} {...data} />
-      ))}
-    </VStack>
+    <LayoutGroup>
+      <MotionV
+        w='full'
+        p='2'
+        borderRadius='lg'
+        border='1px'
+        borderColor='gray.200'
+        shadow='md'
+        bgColor='white'
+        divider={<StackDivider />}
+        layout
+      >
+        <AnimatePresence initial={false}>
+          <AddLocationItem addLocation={addLocation} />
+          {locations.map((data) => (
+            <LocationItem key={data.id} removeLocation={removeLocation} {...data} />
+          ))}
+        </AnimatePresence>
+      </MotionV>
+    </LayoutGroup>
   );
 };
 
